91db647aeac9675a06c5e8eaa4a7f96b938340c4
Ricky Powell <rickypowell@users.noreply.github.com>
2026-03-26 14:33:54 -0500

    chore(OpenLibraryBookFetcher): enhance debug
    - Playground for experimentation with the api
    - log debug json request/response

--- Dewey/Networking/OpenLibraryBookFetcher.swift
+++ Dewey/Networking/OpenLibraryBookFetcher.swift
@@ -1,4 +1,19 @@
 import Foundation
+import os
+import Playgrounds
+
+#Playground {
+    let fetcher = OpenLibraryBookFetcher(networkData: LiveNetworkData())
+    let query = BookQuery(q: "title:The Odessey of Homer", fields: [.title, .authorName, .authorKey, .isbn, .subject], limit: 15, offset: 0)
+    do {
+        let result = try await fetcher.fetch(query)
+        let docs = result.docs
+    } catch {
+        _ = error
+    }
+}
+
+fileprivate let logger = Logger(subsystem: "com.ricky-powell.Dewey", category: "OpenLibraryBookFetcher")
 
 /// This knows how to fetch from Open Library using the search API.
 /// This is a concrete implmentation of the `BookFetcher` protocol.
@@ -20,6 +35,13 @@ class OpenLibraryBookFetcher: BookFetcher {
         }
 
         let (data, _) = try await networkData.data(from: url)
+        
+        #if DEBUG
+        if let str = String(bytes: data, encoding: .utf8) {
+            logger.debug("JSON request: \(url)")
+            logger.debug("JSON response: \(str)")
+        }
+        #endif
 
         let decoder = JSONDecoder()
         return try decoder.decode(BookPagePayload.self, from: data)
@@ -31,6 +53,7 @@ class OpenLibraryBookFetcher: BookFetcher {
     /// - `fields` for the correctly expected fields to retrieve
     /// - `limit` and `offset` for pagination
     func buildFetchURL(_ query: BookQuery) -> URL? {
+        // SAFETY: `baseURL` is verified the correct format
         var components = URLComponents(string: baseURL)!
         components.queryItems = [
             URLQueryItem(name: "q", value: query.q),
